8636bf667fd4bffcc323b08039b267ca9b74138f
Karle <karlresleith@gmail.com>
2019-07-08 22:52:26 +0100

    Created Prototype
    Added TestArea, CameraCtrl, PlayerCtrl and some lighting changes to test out camera positioning and tank controls

---
+++ Evilution/Assets/Scripts/Camera/Camera1.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera1 : MonoBehaviour
+{
+    public GameObject cameraOne;
+    public GameObject cameraTwo;
+    public bool on = false;
+
+    public int camNum;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        camNum = 1;
+        StartCoroutine(SwitchCamera());
+    }
+
+    IEnumerator SwitchCamera()
+    {
+        yield return new WaitForSeconds(3);
+        cameraTwo.SetActive(true);//turn on camera 2
+        cameraOne.SetActive(false);//turn off camera 1
+        on = true;
+        camNum = 2;
+
+        yield return new WaitForSeconds(3);
+        cameraTwo.SetActive(false);//turn on camera 2
+        cameraOne.SetActive(true);//turn off camera 1
+        on = false;
+        camNum = 1;
+
+    }
+
+
+}
---
+++ Evilution/Assets/Scripts/Player/PlayerCtrl.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCtrl : MonoBehaviour
+{
+
+    public GameObject theSTAR;
+    public bool isMoving;
+    public float horizontalMove;
+    public float verticalMove;
+    public float moveSpeed;
+    public float turnSpeed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if player is moving do this 
+        if (Input.GetButton("Horizontal") || Input.GetButton("Vertical"))
+        {
+            isMoving = true;
+            //PLAY ANIMATION for RUNNING
+            horizontalMove = Input.GetAxis("Horizontal") * Time.deltaTime * turnSpeed;
+            verticalMove = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+            theSTAR.transform.Rotate(0, horizontalMove, 0);
+            theSTAR.transform.Translate(0,0, verticalMove);
+
+        }
+        else
+        {
+            isMoving = false;
+        }
+    }
+
+}
